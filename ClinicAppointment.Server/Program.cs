c767c7b3e95c09beb9b37b93e26cc9ac6c2930f3
shilpitha6 <149120524+shilpitha6@users.noreply.github.com>
2026-04-16 22:09:39 -0400

    healthcheck path

--- ClinicAppointment.Server/Program.cs
+++ ClinicAppointment.Server/Program.cs
@@ -15,6 +15,9 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://+:{port}");
+
 // Add services to the container.
 
 //builder.Services.AddControllers();
